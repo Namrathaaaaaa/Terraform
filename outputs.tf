4853d63b50df74881ca20a45c206e1c21a0ddaf8
Namratha <namratha343@gmail.com>
2025-05-13 16:43:56 +0530

    terrfaorm eroor

--- outputs.tf
+++ outputs.tf
@@ -11,19 +11,19 @@
 # }
 
 output "ec2_public_ip" {
-    value = [
-        for key in aws_instance.my_instance : key.public_ip
-    ]
+  value = [
+    for key in aws_instance.my_instance : key.public_ip
+  ]
 }
 
 output "ec2_public_dns" {
-    value = [
-        for key in aws_aws_instance.my_instance : key.public_dns
-    ]
+  value = [
+    for key in aws_instance.my_instance : key.public_dns
+  ]
 }
 
 output "ec2_private_ip" {
-    value = [
-        for key in aws_instance.my_instance : key.private_ip
-    ]
+  value = [
+    for key in aws_instance.my_instance : key.private_ip
+  ]
 }
