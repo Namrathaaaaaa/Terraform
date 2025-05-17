0d2f8d3aaeaf7294d14f5b35e21b2905215da5f4
Namratha <namratha343@gmail.com>
2025-05-17 12:24:59 +0530

    backend

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "5.0.0"
     }
   }
+  backend "s3" {
+    bucket = "my-tws-bucket-namratha"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "tws-state-table"
+  }
 }
