ab316803d7862ba0ddc2ee539496b07cd1db6323
Namratha <namratha343@gmail.com>
2025-05-18 18:04:41 +0530

    version

--- terraform.tf
+++ terraform.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0.0"
+      version = "5.79.0"
     }
   }
   backend "s3" {
-    bucket = "my-tws-bucket-namratha"
-    key = "terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "my-tws-bucket-namratha"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "tws-state-table"
   }
 }
